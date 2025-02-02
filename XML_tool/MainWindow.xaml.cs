92282ded6bb43f423fedbd55ecca0b32db30ad1a
Lukasz <woryszlukasz@gmail.com>
2025-02-02 22:25:17 +0100

    Adding simple navigation

--- XML_tool/MainWindow.xaml.cs
+++ XML_tool/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows;
 using System.Windows.Navigation;
 using System.Xml;
 using XML_tool.Services;
+using System.Windows.Controls;
+using XML_tool.Views;
 
 namespace XML_tool
 {
@@ -12,6 +14,18 @@ namespace XML_tool
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OpenAddXmlWindow(object sender, RoutedEventArgs e)
+        {
+            MainContent.Content = new AddXmlView();
+            ButtonPanel.Visibility = Visibility.Collapsed;
+        }
+
+        private void OpenRemoveXmlWindow(object sender, RoutedEventArgs e) 
+        {
+            MainContent.Content = new RemoveXmlView();
+            ButtonPanel.Visibility = Visibility.Collapsed;
         }
     }
 }
